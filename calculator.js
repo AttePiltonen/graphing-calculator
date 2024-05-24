b3a2af6df2194b16305ef20061d5757c4761cab1
AttePiltonen <79204283+AttePiltonen@users.noreply.github.com>
2024-05-24 19:53:02 -0400

    Add eventListeners for buttons

--- calculator.js
+++ calculator.js
@@ -1,22 +1,56 @@
-function add (a, b) {
-    return a + b;
-  };
-  
-function subtract (a, b) {
-    return a - b;
-  };
+document.addEventListener('DOMContentLoaded', function() {
+    function add (a, b) {
+        return a + b;
+      };
+      
+    function subtract (a, b) {
+        return a - b;
+      };
+    
+    function multiply (a, b) {
+        return a * b;
+      };
+    
+    function divide (a, b) {
+        if (b === 0) {
+            return "Math ERROR";
+        }
+        return a / b;
+      };
+    
+    function operate (a, b, operation) {
+        return operation(a, b);
+      };
+    
+    const displayCalculation = document.querySelector('.calculation');
+    const displaySolution = document.querySelector('.solution');
+    let calculation = '';
+    let solution = '';
 
-function multiply (a, b) {
-    return a * b;
-  };
+    document.querySelector('.buttons-container').addEventListener('click', function(event) {
+        if (event.target.tagName === 'BUTTON') {
+            const value = event.target.textContent;
 
-function divide (a, b) {
-    if (b === 0) {
-        return "Math ERROR";
-    }
-    return a / b;
-  };
+            if (value === 'DEL') {
+                calculation = calculation.slice(0, -1);
+            } else if (value === 'AC') {
+                calculation = '';
+                solution = '';
+            } else if (value === '=') {
+                try {
+                    solution = '';
+                    calculation = solution;
+                } catch (e) {
+                    solution = 'Error';
+                }
+            } else if (value === 'ANS') {
+                calculation += solution;
+            } else {
+                calculation += value;
+            }
 
-function operate (a, b, operation) {
-    return operation(a, b);
-  };
+            displayCalculation.textContent = calculation;
+            displaySolution.textContent = solution;
+        }
+    });
+});

aa69828ea31d7154413ae2f27beb627427cf4f6d
AttePiltonen <79204283+AttePiltonen@users.noreply.github.com>
2024-05-24 19:13:21 -0400

    Create operation functions

---
+++ calculator.js
@@ -0,0 +1,22 @@
+function add (a, b) {
+    return a + b;
+  };
+  
+function subtract (a, b) {
+    return a - b;
+  };
+
+function multiply (a, b) {
+    return a * b;
+  };
+
+function divide (a, b) {
+    if (b === 0) {
+        return "Math ERROR";
+    }
+    return a / b;
+  };
+
+function operate (a, b, operation) {
+    return operation(a, b);
+  };

2bd478dcc8555259454f37a32fba4fc3053cb2c7
AttePiltonen <79204283+AttePiltonen@users.noreply.github.com>
2024-05-25 19:48:20 -0400

    Begin restructuring

--- calculator.js
+++ calculator.js
@@ -22,35 +22,56 @@ document.addEventListener('DOMContentLoaded', function() {
         return operation(a, b);
       };
     
-    const displayCalculation = document.querySelector('.calculation');
+    //const displayCalculation = document.querySelector('.calculation');
     const displaySolution = document.querySelector('.solution');
-    let calculation = '';
-    let solution = '';
+    //let calculation = '';
+    //let solution = '';
+    let num1 = 0;
+    let num2 = 0;
+    let operation;
+    const operation_obj = {'+':add, '-':subtract, 'X':multiply, '/':divide}
 
     document.querySelector('.buttons-container').addEventListener('click', function(event) {
         if (event.target.tagName === 'BUTTON') {
             const value = event.target.textContent;
-
-            if (value === 'DEL') {
-                calculation = calculation.slice(0, -1);
-            } else if (value === 'AC') {
-                calculation = '';
-                solution = '';
-            } else if (value === '=') {
-                try {
-                    solution = '';
-                    calculation = solution;
-                } catch (e) {
-                    solution = 'Error';
-                }
-            } else if (value === 'ANS') {
-                calculation += solution;
-            } else {
-                calculation += value;
+            if (value === 'AC') {
+                num1 = 0;
+                num2 = 0;
+                displaySolution.textContent = 0;
             }
-
-            displayCalculation.textContent = calculation;
-            displaySolution.textContent = solution;
-        }
+            if (value in operation_obj) {
+                operation = operation_obj[value];
+            }
+            if (value === '=') {
+                displaySolution.textContent = operate(num1, num2, operation);
+            }
+        };
     });
+
+    // document.querySelector('.buttons-container').addEventListener('click', function(event) {
+    //     if (event.target.tagName === 'BUTTON') {
+    //         const value = event.target.textContent;
+
+    //         if (value === 'DEL') {
+    //             calculation = calculation.slice(0, -1);
+    //         } else if (value === 'AC') {
+    //             calculation = '';
+    //             solution = '';
+    //         } else if (value === '=') {
+    //             try {
+    //                 solution = '';
+    //                 calculation = solution;
+    //             } catch (e) {
+    //                 solution = 'Error';
+    //             }
+    //         } else if (value === 'ANS') {
+    //             calculation += solution;
+    //         } else {
+    //             calculation += value;
+    //         }
+
+    //         displayCalculation.textContent = calculation;
+    //         displaySolution.textContent = solution;
+    //     }
+    //});
 });
